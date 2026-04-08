8f7acb484f1aaaa8c68757acf788085d6eb3f661
Victor Cechinel <dev.victorcechinel@gmail.com>
2026-04-08 02:04:09 -0300

    feat(sns-subscription): add HTTPS auto-confirmation support

--- modules/sns-subscription/main.tf
+++ modules/sns-subscription/main.tf
@@ -4,4 +4,7 @@ resource "aws_sns_topic_subscription" "this" {
   topic_arn = each.value.topic_arn
   protocol  = each.value.protocol
   endpoint  = each.value.endpoint
+
+  endpoint_auto_confirms    = each.value.protocol == "https" ? true : false
+  confirmation_timeout_in_minutes = each.value.protocol == "https" ? 5 : 1
 }
--- modules/sns-subscription/variables.tf
+++ modules/sns-subscription/variables.tf
@@ -1,5 +1,5 @@
 variable "subscriptions" {
-  description = "(Required) List of SNS subscriptions to create. Use this to subscribe cross-account topics to local endpoints (e.g. Lambda)."
+  description = "(Required) List of SNS subscriptions to create. Use this to subscribe cross-account topics to local endpoints (e.g. Lambda, HTTPS)."
   type = list(object({
     topic_arn = string
     protocol  = string
