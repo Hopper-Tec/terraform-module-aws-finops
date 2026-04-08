7ecc1ce9442644535c338adc757b1f80069cf9da
Victor Cechinel <dev.victorcechinel@gmail.com>
2026-04-08 14:12:35 -0300

    refactor: remove unused sns-subscription module

--- modules/sns-subscription/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "subscription_arns" {
-  description = "Map of subscription ARNs"
-  value       = { for k, v in aws_sns_topic_subscription.this : k => v.arn }
-}
--- modules/sns-subscription/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "subscriptions" {
-  description = "(Required) List of SNS subscriptions to create. Use this to subscribe cross-account topics to local endpoints (e.g. Lambda, HTTPS)."
-  type = list(object({
-    topic_arn = string
-    protocol  = string
-    endpoint  = string
-  }))
-}
